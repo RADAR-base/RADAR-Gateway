206d9d0cdbdade08830ac1e62c47998781b1af74
Joris Borgdorff <joris@thehyve.nl>
2018-06-06 15:38:07 +0200

    Load gateway.yml by default

--- src/main/kotlin/main.kt
+++ src/main/kotlin/main.kt
@@ -4,19 +4,32 @@ import org.radarcns.gateway.Config
 import org.radarcns.gateway.GrizzlyServer
 import java.io.File
 import java.io.IOException
+import java.nio.file.Files
+import java.nio.file.Paths
 import kotlin.system.exitProcess
 
+fun loadConfig(args: Array<String>): Config {
+    val configFileName = when {
+        args.size == 1 -> args[0]
+        Files.exists(Paths.get("gateway.yml")) -> "gateway.yml"
+        else -> null
+    }
+    return if (configFileName != null) {
+        val configFile = File(configFileName)
+        println("Reading configuration from ${configFile.absolutePath}")
+        try {
+            val mapper = ObjectMapper(YAMLFactory())
+            mapper.readValue(configFile, Config::class.java)
+        } catch (ex: IOException) {
+            println("Usage: radar-gateway [config.yml]")
+            println("Failed to read config file $configFile: ${ex.message}")
+            exitProcess(1)
+        }
+    } else Config()
+}
+
 fun main(args: Array<String>) {
-    val config = if (args.size == 1) {
-            try {
-                val mapper = ObjectMapper(YAMLFactory())
-                mapper.readValue(File(args[0]), Config::class.java)
-            } catch (ex: IOException) {
-                println("Usage: radar-gateway [config.yml]")
-                println("Failed to read config file ${args[0]}: ${ex.message}")
-                exitProcess(1)
-            }
-        } else Config()
+    val config = loadConfig(args)
 
     val server = GrizzlyServer(config)
     try {
