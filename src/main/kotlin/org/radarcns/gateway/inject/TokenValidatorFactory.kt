5af40d644874c5dbbd8993c1a1bfac445340773a
Joris Borgdorff <joris@thehyve.nl>
2018-08-09 15:51:13 +0200

    Updated MP keystore

--- src/main/kotlin/org/radarcns/gateway/inject/TokenValidatorFactory.kt
+++ src/main/kotlin/org/radarcns/gateway/inject/TokenValidatorFactory.kt
@@ -3,34 +3,22 @@ package org.radarcns.gateway.inject
 import org.radarcns.auth.authentication.TokenValidator
 import org.radarcns.auth.config.YamlServerConfig
 import org.radarcns.gateway.Config
-import org.slf4j.LoggerFactory
 import java.net.URI
-import java.net.URISyntaxException
 import java.util.function.Supplier
 import javax.ws.rs.core.Context
 
 class TokenValidatorFactory: Supplier<TokenValidator> {
-
     @Context
     private lateinit var config: Config
 
     override fun get(): TokenValidator {
-        val publicKey = try {
-            URI("${config.managementPortalUrl}/oauth/token_key")
-        } catch (e: URISyntaxException) {
-            logger.info("Failed to load Management Portal URL ${config.managementPortalUrl}", e)
-            null
-        }
-
-        return if (publicKey == null) TokenValidator() else {
+        return try {
+            TokenValidator()
+        } catch (e: RuntimeException) {
             val cfg = YamlServerConfig()
-            cfg.publicKeyEndpoints = listOf(publicKey)
+            cfg.publicKeyEndpoints = listOf(URI("${config.managementPortalUrl}/oauth/token_key"))
             cfg.resourceName = "res_gateway"
             TokenValidator(cfg)
         }
-    }
-
-    companion object {
-        private val logger = LoggerFactory.getLogger(TokenValidatorFactory::class.java)
     }
 }
