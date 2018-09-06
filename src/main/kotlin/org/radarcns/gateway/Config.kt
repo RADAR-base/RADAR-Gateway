aafabef5b36242d6d5603aa50cfbf125d3e93250
Joris Borgdorff <joris@thehyve.nl>
2018-09-06 16:09:05 +0200

    Make resource config configurable

--- src/main/kotlin/org/radarcns/gateway/Config.kt
+++ src/main/kotlin/org/radarcns/gateway/Config.kt
@@ -1,5 +1,6 @@
 package org.radarcns.gateway;
 
+import org.radarcns.gateway.inject.ManagementPortalGatewayResources
 import java.net.URI
 
 class Config {
@@ -7,4 +8,5 @@ class Config {
     var restProxyUrl: String = "http://rest-proxy-1:8082"
     var schemaRegistryUrl: String = "http://schema-registry-1:8081"
     var managementPortalUrl: String = "http://managementportal-app:8080/managementportal/"
+    var resourceConfig: Class<*> = ManagementPortalGatewayResources::class.java
 }
--- src/main/kotlin/org/radarcns/gateway/GrizzlyServer.kt
+++ src/main/kotlin/org/radarcns/gateway/GrizzlyServer.kt
@@ -2,32 +2,17 @@ package org.radarcns.gateway
 
 import org.glassfish.grizzly.http.server.HttpServer
 import org.glassfish.jersey.grizzly2.httpserver.GrizzlyHttpServerFactory
-import org.glassfish.jersey.server.ResourceConfig
-import org.radarcns.gateway.auth.ManagementPortalAuthenticationFilter
-import org.radarcns.gateway.inject.GatewayBinder
+import org.radarcns.gateway.inject.GatewayResources
 import java.util.concurrent.TimeUnit
 
-open class GrizzlyServer(private val config: Config) {
+class GrizzlyServer(private val config: Config) {
     private var httpServer: HttpServer? = null
 
-    private fun getResourceConfig(): ResourceConfig {
-        val resources = ResourceConfig()
-        resources.packages(
-                "org.radarcns.gateway.exception",
-                "org.radarcns.gateway.filter",
-                "org.radarcns.gateway.io",
-                "org.radarcns.gateway.resource")
-        resources.register(GatewayBinder(config))
-        registerAuthentication(resources)
-        return resources
-    }
-
-    protected fun registerAuthentication(resources: ResourceConfig) {
-        resources.register(ManagementPortalAuthenticationFilter::class.java)
-    }
-
     fun start() {
-        httpServer = GrizzlyHttpServerFactory.createHttpServer(config.baseUri, getResourceConfig())
+        val resourceConfig = (config.resourceConfig.newInstance() as GatewayResources)
+                .getResources(config)
+
+        httpServer = GrizzlyHttpServerFactory.createHttpServer(config.baseUri, resourceConfig)
         httpServer!!.start()
     }
 
