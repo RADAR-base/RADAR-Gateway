e1a7fb9bf8a88f458f14d383a5519163bd3e4258
Joris Borgdorff <github@blootsvoets.net>
2018-11-15 10:02:43 +0100

    Proper user and project permission check

--- src/main/kotlin/org/radarcns/gateway/auth/KeycloakAuth.kt
+++ src/main/kotlin/org/radarcns/gateway/auth/KeycloakAuth.kt
@@ -9,11 +9,14 @@ import javax.ws.rs.ForbiddenException
  * Parsed JWT for validating authorization of data contents.
  */
 class KeycloakAuth(project: String?, private val token: DecodedJWT) : Auth {
-    override val defaultProject = token.getClaim("project").asString() ?: project
-    override val userId: String? = if (token.subject.isNotEmpty()) token.subject else null
+    private val claimProject = token.getClaim("project").asString()
+    override val defaultProject = claimProject ?: project
+    override val userId: String? = token.subject?.takeUnless { it.isEmpty() }
 
     override fun checkPermission(projectId: String?, userId: String?, sourceId: String?) {
-        if (!hasPermission(MEASUREMENT_CREATE)) {
+        if (!hasPermission(MEASUREMENT_CREATE)
+                || (userId != null && this.userId != null && userId != this.userId)
+                || (projectId != null && claimProject != null && projectId != claimProject)) {
             throw ForbiddenException("No permission to create measurement for " +
                     "project $projectId with user $userId and source $sourceId " +
                     "using token ${token.token}")
