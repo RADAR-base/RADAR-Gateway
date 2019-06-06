295c1b1ee20d4aef11af63c9a10d6e214cc49bc5
Joris Borgdorff <github@blootsvoets.net>
2019-06-06 10:25:31 +0200

    Split out size limit from decompression

--- src/main/kotlin/org/radarcns/gateway/io/DecompressInterceptor.kt
+++ src/main/kotlin/org/radarcns/gateway/io/DecompressInterceptor.kt
@@ -3,11 +3,15 @@ package org.radarcns.gateway.io
 import org.radarbase.io.lzfse.LZFSEInputStream
 import org.radarcns.gateway.Config
 import org.radarcns.gateway.inject.ProcessAvro
+import org.radarcns.gateway.util.Json.jsonErrorResponse
+import java.util.*
 import java.util.zip.GZIPInputStream
 import javax.annotation.Priority
 import javax.inject.Singleton
+import javax.ws.rs.BadRequestException
 import javax.ws.rs.Priorities
 import javax.ws.rs.core.Context
+import javax.ws.rs.core.Response
 import javax.ws.rs.ext.Provider
 import javax.ws.rs.ext.ReaderInterceptor
 import javax.ws.rs.ext.ReaderInterceptorContext
@@ -20,15 +24,18 @@ import javax.ws.rs.ext.ReaderInterceptorContext
 @ProcessAvro
 @Singleton
 @Priority(Priorities.ENTITY_CODER)
-class DecompressInterceptor(@Context private val config: Config) : ReaderInterceptor {
+class DecompressInterceptor : ReaderInterceptor {
     override fun aroundReadFrom(context: ReaderInterceptorContext): Any {
-        val encoding = context.headers?.getFirst("Content-Encoding")?.toLowerCase()
-        if (encoding == "gzip") {
-            context.inputStream = GZIPInputStream(context.inputStream)
-        } else if (encoding == "lzfse") {
-            context.inputStream = LZFSEInputStream(context.inputStream)
+        val encoding = context.headers?.getFirst("Content-Encoding")?.toLowerCase(Locale.US)
+        context.inputStream = when(encoding) {
+            "gzip" -> GZIPInputStream(context.inputStream)
+            "lzfse" -> LZFSEInputStream(context.inputStream)
+            "identity", null -> context.inputStream
+            else -> throw BadRequestException(
+                    jsonErrorResponse(Response.Status.BAD_REQUEST, "unknown_encoding",
+                            "Encoding $encoding unknown." +
+                                    " This server supports gzip, lzfse and identity compression"))
         }
-        context.inputStream = LimitedInputStream(context.inputStream, config.maxRequestSize)
         return context.proceed()
     }
 }
