8e34c073b9dc0a18c6c7dc24961307bbb915b620
Joris Borgdorff <github@blootsvoets.net>
2019-10-07 15:34:18 +0200

    Fix types

--- src/main/kotlin/org/radarbase/gateway/Config.kt
+++ src/main/kotlin/org/radarbase/gateway/Config.kt
@@ -1,6 +1,7 @@
 package org.radarbase.gateway
 
 import org.radarbase.gateway.inject.ManagementPortalEnhancerFactory
+import org.radarbase.jersey.config.EnhancerFactory
 import java.net.URI
 
 class Config {
@@ -8,7 +9,7 @@ class Config {
     var restProxyUrl: String = "http://rest-proxy-1:8082"
     var schemaRegistryUrl: String = "http://schema-registry-1:8081"
     var managementPortalUrl: String = "http://managementportal-app:8080/managementportal/"
-    var resourceConfig: Class<*> = ManagementPortalEnhancerFactory::class.java
+    var resourceConfig: Class<out EnhancerFactory> = ManagementPortalEnhancerFactory::class.java
     var jwtKeystorePath: String? = null
     var jwtKeystoreAlias: String? = null
     var jwtKeystorePassword: String? = null
