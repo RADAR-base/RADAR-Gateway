109e4658cb0e77116119a0c7f419c511d8c9ba86
Joris Borgdorff <joris@thehyve.nl>
2021-03-17 16:33:57 +0100

    Fix warning

--- deprecated-javax/build.gradle.kts
+++ deprecated-javax/build.gradle.kts
@@ -9,27 +9,28 @@ repositories {
 }
 
 dependencies {
-    compile("org.apache.kafka:kafka-clients:${project.property("kafkaVersion")}") {
+    implementation("org.apache.kafka:kafka-clients:${project.property("kafkaVersion")}") {
         isTransitive = true
         exclude(group = "org.slf4j", module = "slf4j-api")
     }
-    compile("io.confluent:kafka-avro-serializer:${project.property("confluentVersion")}") {
+    implementation("io.confluent:kafka-avro-serializer:${project.property("confluentVersion")}") {
         isTransitive = false
     }
-    compile("io.confluent:kafka-schema-serializer:${project.property("confluentVersion")}") {
+    implementation("io.confluent:kafka-schema-serializer:${project.property("confluentVersion")}") {
         isTransitive = false
     }
-    compile("io.confluent:kafka-schema-registry-client:${project.property("confluentVersion")}") {
+    implementation("io.confluent:kafka-schema-registry-client:${project.property("confluentVersion")}") {
         isTransitive = false
     }
-    compile("org.glassfish.jersey.core:jersey-common:2.31")
-    compile("io.swagger:swagger-annotations:1.6.2")
-    compile("io.confluent:common-utils:6.1.0") {
+    implementation("org.glassfish.jersey.core:jersey-common:2.31")
+    implementation("io.swagger:swagger-annotations:1.6.2")
+    implementation("io.confluent:common-utils:6.1.0") {
         isTransitive = false
     }
 }
 
 tasks.shadowJar {
+    configurations = listOf(project.configurations.compileClasspath.get())
     relocate("javax.ws.rs", "shadow.javax.ws.rs")
     relocate("org.glassfish", "shadow.org.glassfish")
 }
