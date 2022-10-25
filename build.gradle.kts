752e3695cf9d8d2aaa4dbedfb8bcd1786a7a3e5a
mpgxvii <mpgxvii@gmail.com>
2022-10-25 18:54:31 +0100

    Bump version

--- build.gradle.kts
+++ build.gradle.kts
@@ -14,7 +14,7 @@ description = "RADAR Gateway to handle secured data flow to backend."
 
 allprojects {
     group = "org.radarbase"
-    version = "0.5.14-SNAPSHOT"
+    version = "0.5.14"
 
     repositories {
         mavenCentral()
