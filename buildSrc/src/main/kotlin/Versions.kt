1b5a23ac737e5b7f60b164f05cc9ecc2208a90fe
Bastiaan <bastiaan@thehyve.nl>
2024-06-11 17:10:50 +0200

    Bump radar dependencies to and project version to non-snapshot

--- buildSrc/src/main/kotlin/Versions.kt
+++ buildSrc/src/main/kotlin/Versions.kt
@@ -1,6 +1,6 @@
 @Suppress("ConstPropertyName")
 object Versions {
-    const val project = "0.7.2-SNAPSHOT"
+    const val project = "0.7.2"
 
     const val java = 17
     const val kotlin = "1.9.22"
@@ -8,8 +8,8 @@ object Versions {
 
     const val ktor = "2.3.10"
     const val radarJersey = "0.11.1"
-    const val radarCommons = "1.1.3-SNAPSHOT"
-    const val radarSchemas = "0.8.8"
+    const val radarCommons = "1.1.3"
+    const val radarSchemas = "0.8.10"
     const val jackson = "2.15.3"
     const val slf4j = "2.0.13"
     const val log4j2 = "2.23.1"
