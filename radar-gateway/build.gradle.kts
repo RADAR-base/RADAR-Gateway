85b2f7fed1a11f7266d6fbdb002b1b7fa324af51
pvannierop <pim@thehyve.nl>
2024-10-04 15:15:45 +0200

    Add support for Sentry monitoring
    Includes addition to documentation.

--- radar-gateway/build.gradle.kts
+++ radar-gateway/build.gradle.kts
@@ -4,6 +4,9 @@ plugins {
     application
     kotlin("plugin.serialization") version Versions.kotlin
     id("com.avast.gradle.docker-compose") version Versions.dockerCompose
+    // TODO Remove this when new release of radar-commons is available and used in this project.
+    // This version has Sentry support built in for radar-kotlin plugin.
+    id("io.sentry.jvm.gradle") version "4.11.0"
 }
 
 description = "RADAR Gateway to handle secured data flow to backend."
