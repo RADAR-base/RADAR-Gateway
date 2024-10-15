ba1b6568c2d780cf4c110d75a4db7049edb0de6e
Pauline Conde <mpgxvii@gmail.com>
2024-10-16 00:53:10 +0800

    Bump version

--- buildSrc/src/main/kotlin/Versions.kt
+++ buildSrc/src/main/kotlin/Versions.kt
@@ -1,6 +1,6 @@
 @Suppress("ConstPropertyName")
 object Versions {
-    const val project = "0.7.3-SNAPSHOT"
+    const val project = "0.7.4"
 
     const val java = 17
     const val kotlin = "1.9.22"
