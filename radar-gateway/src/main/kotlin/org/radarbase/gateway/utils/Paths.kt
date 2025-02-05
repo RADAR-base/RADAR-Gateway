9be4953d13cbd216dcb417a4a32f169084b1a9f2
this-Aditya <adityamishra2217@gmail.com>
2025-02-05 12:35:31 +0530

    Path utils added

---
+++ radar-gateway/src/main/kotlin/org/radarbase/gateway/utils/Paths.kt
@@ -0,0 +1,9 @@
+package org.radarbase.gateway.utils
+
+object Paths {
+    const val PROJECT_PATH = "projects"
+    const val USER_PATH = "users"
+    const val TOPIC_PATH = "topics"
+    const val FILE_PATH = "files"
+    const val UPLOAD_PATH = "upload"
+}
