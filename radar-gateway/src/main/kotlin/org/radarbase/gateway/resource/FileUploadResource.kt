0e3e757e3d8ee64b9af9c5f0e5b3880649842668
this-Aditya <adityamishra2217@gmail.com>
2025-02-06 14:44:40 +0530

    Using form params instead of path params

--- radar-gateway/src/main/kotlin/org/radarbase/gateway/resource/FileUploadResource.kt
+++ radar-gateway/src/main/kotlin/org/radarbase/gateway/resource/FileUploadResource.kt
@@ -1,43 +1,51 @@
 package org.radarbase.gateway.resource
 
+import jakarta.ws.rs.Consumes
 import jakarta.ws.rs.POST
 import jakarta.ws.rs.Path
-import jakarta.ws.rs.PathParam
 import jakarta.ws.rs.core.Context
+import jakarta.ws.rs.core.MediaType
 import jakarta.ws.rs.core.Response
 import org.glassfish.jersey.media.multipart.FormDataContentDisposition
 import org.glassfish.jersey.media.multipart.FormDataParam
 import org.radarbase.gateway.inject.ProcessFileUpload
 import org.radarbase.gateway.service.storage.StorageService
-import org.radarbase.gateway.utils.Paths.FILE_PATH
-import org.radarbase.gateway.utils.Paths.PROJECT_PATH
-import org.radarbase.gateway.utils.Paths.TOPIC_PATH
-import org.radarbase.gateway.utils.Paths.UPLOAD_PATH
-import org.radarbase.gateway.utils.Paths.USER_PATH
 import org.slf4j.LoggerFactory
 import java.io.InputStream
 import java.net.URI
 
-@Path("")
+@Path("/upload")
 class FileUploadResource(
     @Context private val storageService: StorageService? = null,
 ) {
+
     @ProcessFileUpload
     @POST
-    @Path("/$PROJECT_PATH/{projectId}/$USER_PATH/{subjectId}/$FILE_PATH/$TOPIC_PATH/{topicId}/$UPLOAD_PATH")
+    @Consumes(MediaType.MULTIPART_FORM_DATA)
     fun uploadFileForSubject(
         @FormDataParam("file") fileInputStream: InputStream,
         @FormDataParam("file") fileInfo: FormDataContentDisposition,
-        @PathParam("projectId") projectId: String,
-        @PathParam("subjectId") subjectId: String,
-        @PathParam("topicId") topicId: String,
+        @FormDataParam("projectId") projectId: String,
+        @FormDataParam("subjectId") subjectId: String,
+        @FormDataParam("topicId") topicId: String,
     ): Response {
         if (storageService == null) {
             logger.debug("File uploading is disabled")
             return Response.noContent().build()
         }
-        val filePath = storageService.store(fileInputStream, fileInfo.fileName, projectId, subjectId, topicId)
-        logger.debug("Storing file for project: {}, subject: {}, topic: {}", projectId, subjectId, topicId)
+        val filePath = storageService.store(
+            fileInputStream,
+            fileInfo.fileName,
+            projectId,
+            subjectId,
+            topicId,
+        )
+        logger.debug(
+            "Storing file for project: {}, subject: {}, topic: {}",
+            projectId,
+            subjectId,
+            topicId,
+        )
         return Response.created(URI(filePath)).build()
     }
 
