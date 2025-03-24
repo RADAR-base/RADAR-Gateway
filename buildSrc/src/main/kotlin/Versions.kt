582c3f54b0fd426bae6a420a65a9bffe6da8a45f
EIjo <eljo@live.nl>
2025-03-24 12:24:49 +0100

    Update radarCommons to version 1.2.2

--- buildSrc/src/main/kotlin/Versions.kt
+++ buildSrc/src/main/kotlin/Versions.kt
@@ -8,7 +8,7 @@ object Versions {
 
     const val ktor = "2.3.10"
     const val radarJersey = "0.11.1"
-    const val radarCommons = "1.1.3"
+    const val radarCommons = "1.2.2"
     const val radarSchemas = "0.8.9"
     const val jackson = "2.15.3"
     const val log4j2 = "2.23.1"
