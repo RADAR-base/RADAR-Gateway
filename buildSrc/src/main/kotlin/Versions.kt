1dc79e7bdc0b08786b0296a0223ed7f74544b0ca
Pauline <mpgxvii@gmail.com>
2026-01-16 22:47:54 +0800

    Bump deps with vulnerabilities

--- buildSrc/src/main/kotlin/Versions.kt
+++ buildSrc/src/main/kotlin/Versions.kt
@@ -19,7 +19,7 @@ object Versions {
     const val avro = "1.11.4"
     const val confluent = "7.6.0"
     const val kafka = "$confluent-ce"
-    const val minio = "8.5.10"
+    const val minio = "8.6.0"
     const val multipart = "3.1.10"
 
     const val mockitoKotlin = "5.3.1"
