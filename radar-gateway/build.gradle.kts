e01f33b58f77e33b6e5852581263dfd9f211fe92
Pauline <mpgxvii@gmail.com>
2026-01-20 21:40:36 +0800

    Increase coroutine IO dispatcher parallelism

--- radar-gateway/build.gradle.kts
+++ radar-gateway/build.gradle.kts
@@ -18,6 +18,8 @@ application {
         "-Dcom.sun.management.jmxremote.port=9010",
         "-Dcom.sun.management.jmxremote.authenticate=false",
         "-Dcom.sun.management.jmxremote.ssl=false",
+        // Increase coroutine IO dispatcher parallelism
+        "-Dkotlinx.coroutines.io.parallelism=128",
     )
 }
 
