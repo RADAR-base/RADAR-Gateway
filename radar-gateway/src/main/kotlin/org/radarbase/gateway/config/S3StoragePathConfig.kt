11641aaa0341b60c7f70d6640477bd7e509e7fb6
this-Aditya <adityamishra2217@gmail.com>
2026-01-22 18:49:40 +0000

    Minor fixes

--- radar-gateway/src/main/kotlin/org/radarbase/gateway/config/S3StoragePathConfig.kt
+++ radar-gateway/src/main/kotlin/org/radarbase/gateway/config/S3StoragePathConfig.kt
@@ -1,6 +1,6 @@
 package org.radarbase.gateway.config
 
-import org.radarbase.gateway.utils.Env.AWS_S3_BUCKET_NAME
+import org.radarbase.gateway.utils.Env.AWS_S3_PATH_PREFIX
 import org.radarbase.jersey.config.ConfigLoader.copyEnv
 
 data class S3StoragePathConfig(
@@ -8,7 +8,7 @@ data class S3StoragePathConfig(
     val collectPerDay: Boolean = true,
 ) {
     fun withEnv(): S3StoragePathConfig = this
-        .copyEnv(AWS_S3_BUCKET_NAME) {
-            copy(prefix = prefix)
+        .copyEnv(AWS_S3_PATH_PREFIX) {
+            copy(prefix = it)
         }
 }
