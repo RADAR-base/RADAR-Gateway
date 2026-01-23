4d5153da38b98e405ff27d5c551fafb789eef2f1
this-Aditya <adityamishra2217@gmail.com>
2026-01-23 12:38:16 +0000

    Misc changes

--- radar-gateway/src/main/kotlin/org/radarbase/gateway/config/S3StorageConfig.kt
+++ radar-gateway/src/main/kotlin/org/radarbase/gateway/config/S3StorageConfig.kt
@@ -16,29 +16,18 @@ data class S3StorageConfig(
     val region: String? = null,
     val path: S3StoragePathConfig = S3StoragePathConfig(),
 ) {
-    fun withEnv(): S3StorageConfig = this
-        .copyEnv(AWS_ENDPOINT_URL_S3) {
+    fun withEnv(): S3StorageConfig = this.copyEnv(AWS_ENDPOINT_URL_S3) {
             copy(url = it)
-        }
-        .copyEnv(AWS_ACCESS_KEY_ID) {
+        }.copyEnv(AWS_ACCESS_KEY_ID) {
             copy(accessKey = it)
-        }
-        .copyEnv(AWS_SECRET_ACCESS_KEY) {
+        }.copyEnv(AWS_SECRET_ACCESS_KEY) {
             copy(secretKey = it)
-        }
-        .copyEnv(AWS_S3_BUCKET_NAME) {
+        }.copyEnv(AWS_S3_BUCKET_NAME) {
             copy(bucketName = it)
-        }
-        .copyEnv(AWS_DEFAULT_REGION) {
+        }.copyEnv(AWS_DEFAULT_REGION) {
             copy(region = it)
+        }.copyOnChange(path, S3StoragePathConfig::withEnv) {
+            copy(path = it)
         }
-        .copyOnChange(
-            path,
-            {
-                it.withEnv()
-            },
-            {
-                copy(path = it)
-            },
-        )
+
 }
